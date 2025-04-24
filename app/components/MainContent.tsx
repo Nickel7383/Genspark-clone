f3d710308ffe7dde8e0a3a30531be41dfa96bd87
Nickel7383 <134693771+Nickel7383@users.noreply.github.com>
2025-04-24 13:21:17 +0900

    Update MainContent.tsx

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -28,7 +28,7 @@ export default function MainContent() {
       <div className='max-w-[800px] w-full mx-auto flex-1 flex flex-col h-full'>
         {messages.length === 0 ? (
           <div className="flex-1 flex flex-col items-center justify-center">
-            <h2 className="text-3xl font-bold mb-6 text-white">AI 에이전트1</h2>
+            <h2 className="text-3xl font-bold mb-6 text-white">AI 에이전트</h2>
             <div className="w-full">
               <ChatInput onSendMessage={handleNewMessage} messages={messages} />
             </div>
